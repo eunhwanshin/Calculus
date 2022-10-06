d6994490c6b72067e9c70852608d061e245d5dfb
eunhwanshin <ehshin@gmail.com>
2022-10-05 18:44:28 -0700

    Exercise 8.7-37

--- ex_08_7_37.m
+++ ex_08_7_37.m
@@ -2,32 +2,35 @@
 
 clear
 
+% We can obtain P_8(x) by applying the followings:
+%
 % pkg load symbolic
+% syms x
+% f = cos(x)
+% taylor(f, x, 0, 'order', 9)
+%
+% The result is
+%
+%     8      6    4    2
+%    x      x    x    x
+%  ----- - --- + -- - -- + 1
+%  40320   720   24   2
 
-syms x
-f = cos(x)
+f = @(x) cos(x);
 
-P2 = taylor(f, x, 0, 'order', 3)
-P4 = taylor(f, x, 0, 'order', 5)
-P6 = taylor(f, x, 0, 'order', 7)
-P8 = taylor(f, x, 0, 'order', 9)
+P2 = @(x) 1 - x.^2 / 2;
+P4 = @(x) 1 - x.^2 / 2 + x.^4 / 24;
+P6 = @(x) 1 - x.^2 / 2 + x.^4 / 24 - x.^6 / 720;
+P8 = @(x) 1 - x.^2 / 2 + x.^4 / 24 - x.^6 / 720 + x.^8 / 40320;
 
 v = [-2:0.01:2] * pi;
-y = subs(f, x, v);
-plot(v, double(y))
+
+plot(v, f(v))
 hold on
-y = subs(P2, x, v);
-plot(v, double(y))
-
-y = subs(P4, x, v);
-plot(v, double(y))
-
-y = subs(P6, x, v);
-plot(v, double(y))
-
-y = subs(P8, x, v);
-plot(v, double(y))
-
+plot(v, P2(v))
+plot(v, P4(v))
+plot(v, P6(v))
+plot(v, P8(v))
 hold off
 ylim([-6 6])
 grid on
